7f61a41df09c3ffb7a06ec969202449749a444a3
Lee Castelani <notayogilee@gmail.com>
2020-08-30 09:17:43 -0400

    initial commit

---
+++ build/precache-manifest.96a75c6f680a5eecee1ca8c3f43ddc15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "633e6e02e48d3da8845325e766ae898b",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0a9e3aa77f1470a251cb",
+    "url": "./static/js/2.bf62a0f7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.bf62a0f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9e855bd4a26041f8bf3",
+    "url": "./static/js/main.7818ec48.chunk.js"
+  },
+  {
+    "revision": "72cac9d83a652efe19ce",
+    "url": "./static/js/runtime-main.888e38f2.js"
+  }
+]);
